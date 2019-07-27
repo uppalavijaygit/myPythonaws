7c3589486c9784d5e595bb77e423b974eb11260d
Vijay Kumar Uppala <vijaykumaruppala@Vijays-MacBook-Air.local>
2019-07-27 13:43:40 +0100

    first commit code

---
+++ code/awsEc2SanpShot.py
@@ -0,0 +1,11 @@
+import boto3
+
+session =  boto3.Session(profile_name='vjcloud')
+ec2= session.resource('ec2')
+
+def list_instances():
+    for i in ec2.instances.all():
+        print(i)
+
+if __name__ =='__main__':
+    list_instances()
