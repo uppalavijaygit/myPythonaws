a3031e82cd466e5446cbfe231e44ab88e8aba49a
Vijay Kumar Uppala <vijaykumaruppala@Vijays-MacBook-Air.local>
2019-07-27 16:16:52 +0100

    updated

--- code/awsEc2SanpShot.py
+++ code/awsEc2SanpShot.py
@@ -1,11 +1,16 @@
 import boto3
+import click
+
 
 session =  boto3.Session(profile_name='vjcloud')
 ec2= session.resource('ec2')
 
+@click.command()
 def list_instances():
+    "List Instances"
     for i in ec2.instances.all():
-        print(i)
+        print(','.join((i.id, i.instance_type,i.placement['AvailabilityZone'],i.state['Name'],i.public_dns_name)))
+
 
 if __name__ =='__main__':
     list_instances()
